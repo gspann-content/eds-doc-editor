ed2c85c3a72a322d049f8c065274955ba08c29b6
gspann-content <gspann.content@gmail.com>
2024-10-08 15:11:13 +0530

    Add changes

--- blocks/page-title/page-title.js
+++ blocks/page-title/page-title.js
@@ -1,15 +1,18 @@
 export default function decorate(block) {
   const twDiv = document.createElement('div');
   twDiv.classList.add('tw');
+
   const rows = [...block.children];
+
   // access the rows (image in row 1, text in row2 )
   if (rows.length === 2) {
     const pagetitleitem = document.createElement('div');
-    pagetitleitem.classList = ('pagetitleitem flex  flex-grow-1 items-center bg[#e4f1f5] shadow-md mb-4');
+    pagetitleitem.classList = ('pagetitleitem items-center bg[#e4f1f5] shadow-md mb-4 ');
 
     // Create image and text divs
     const imageDiv = document.createElement('div');
     const textDiv = document.createElement('div');
+
     // append the 1st row content to the image div and 2nd row content to the text div
     imageDiv.appendChild(rows[0].children[0]);
     textDiv.appendChild(rows[1].children[0]);
@@ -17,9 +20,11 @@ export default function decorate(block) {
     if (imageDiv && textDiv) {
       imageDiv.classList.add('page-title-image');
       textDiv.classList.add('page-title-text');
+
       // append the image and text divs to the parent divs.
       pagetitleitem.appendChild(imageDiv);
       pagetitleitem.appendChild(textDiv);
+
       // add the contents to the block
       block.appendChild(pagetitleitem);
     }
