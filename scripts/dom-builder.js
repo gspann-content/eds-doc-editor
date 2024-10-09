5163d48942d6488a2c5913a90c332444fde409bd
Nagaraju Koniki <nagaraju.koniki@gspann.com>
2024-10-09 10:27:55 +0530

    updated breadcrumb code

--- scripts/dom-builder.js
+++ scripts/dom-builder.js
@@ -86,7 +86,8 @@ export function tr(...items) { return domEl('tr', ...items); }
 export function td(...items) { return domEl('td', ...items); }
 export function th(...items) { return domEl('th', ...items); }
 export function time(...items) { return domEl('time', ...items); }
-
+export function path(...items) { return domEl('path', ...items)}
+export function svg(...items) { return domEl('svg', ...items); }
 export function checkbox(attributes, labelContent) {
   const checkboxInput = input({ type: 'checkbox', ...attributes });
   const labelElement = label(labelContent, checkboxInput);
