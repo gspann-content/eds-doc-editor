e535499758156526d28629b02d3a5bfcfdae2531
nagaraju.koniki <nagaraju.koniki@gmail.com>
2024-10-14 14:37:44 +0530

    fixed slit issues

--- blocks/headerbar/headerbar.js
+++ blocks/headerbar/headerbar.js
@@ -1,5 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const primaryDiv = document.createElement('div');
   primaryDiv.className = 'tw-flex tw-w-full tw-bg-grey-900 tw-text-grey-300 tw-z-[100] tw-relative tw-justify-between';
@@ -14,46 +12,43 @@ export default function decorate(block) {
 
   const blockSize = [...block.children].length;
 
-  [...block.children].forEach((row,index) => {
-    
-    if(index===0){
+  [...block.children].forEach((row, index) => {
+    if (index === 0) {
       const headerbar = document.querySelector('.headerbar p');
       headerbar.classList.add('tw-mb-0');
       logo.append(row);
- 
-    }
-    else if(index >= 1 && index <= (blockSize-2)) {
+    } else if (index >= 1 && index <= (blockSize - 2)) {
       row.className = 'tw-inline-flex tw-items-center tw-mr-8';
-      
+
       links.append(row);
       [...row.children].forEach((child) => {
         // Remove 'button-container' class
-        if (child.classList.contains('button-container' ))  {
+        if (child.classList.contains('button-container')) {
           child.classList.remove('button-container');
           child.classList.add('cta-container');
 
           const parEffect = row.querySelector('.cta-container p');
           parEffect.classList.add('tw-mb-0');
-          
+
           const iconEffect = links.querySelectorAll('img');
-          iconEffect.forEach(imgTag => {
+          iconEffect.forEach((imgTag) => {
             imgTag.classList.add(...'tw-text-grey-300 tw-mr-8'.split(' '));
-          })
+          });
           const anchorEffect = row.querySelector('.cta-container a');
           anchorEffect.classList.add(...'tw-text-grey-300 hover:tw-text-white tw-ml-16'.split(' '));
-          anchorEffect.classList.add('tw-flex', 'tw-items-center'); 
+          anchorEffect.classList.add('tw-flex', 'tw-items-center');
         }
         const linkItems = links.querySelectorAll('.tw-inline-flex.tw-items-center.tw-mr-8');
         linkItems.forEach((linkItem) => {
           // Select the <img> tag within the <picture>
           const imgTag = linkItem.querySelector('img');
           // Select the anchor tag
-         const anchor = linkItem.querySelector('a');
-         if (imgTag && anchor) {
+          const anchor = linkItem.querySelector('a');
+          if (imgTag && anchor) {
           // Insert the <img> tag below the anchor tag
-          anchor.insertAdjacentElement('afterbegin', imgTag);
-        }
-      });
+            anchor.insertAdjacentElement('afterbegin', imgTag);
+          }
+        });
         // Update the anchor tag
         const anchor = child.querySelector('a');
         if (anchor) {
@@ -61,9 +56,8 @@ export default function decorate(block) {
           anchor.classList.add('cta'); // Add 'cta' class
           anchor.removeAttribute('title');
         }
-      })
-    }
-    else if(index === (blockSize-1)) {
+      });
+    } else if (index === (blockSize - 1)) {
       resourceForm.append(row);
       [...row.children].forEach((child) => {
         // Remove 'button-container' class
@@ -79,19 +73,16 @@ export default function decorate(block) {
           anchor.className = 'tw-text-mobBase md:tw-text-base tw-flex tw-items-center tw-whitespace-nowrap focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-blue-700 tw-rounded tw-border tw-py-12 tw-px-16 md:tw-px-20 active:tw-bg-blue-900 tw-border-blue-700 tw-text-white tw-bg-gradient-to-r tw-bg-blue-700 tw-from-blue-800 tw-via-blue-800 tw-to-blue-800 tw-bg-bottom tw-bg-no-repeat tw-bg-[length:100%_0px] hover:tw-bg-blue-800 hover:tw-text-gray-200 tw-transition-all tw-h-full tw-rounded-none lg:tw-px-32 tw-py-20'; // Add 'cta' class
           anchor.removeAttribute('title');
         }
-      })
+      });
       const btnContainer = row.querySelector('.resourceForm p');
       btnContainer.classList.add('tw-mb-0');
     }
-    
-  })
+  });
 
   linksContainer.append(links);
   linksContainer.append(resourceForm);
-
   primaryDiv.append(logo);
   primaryDiv.append(linksContainer);
-
   block.textContent = '';
   block.classList.add('tw');
   block.appendChild(primaryDiv);
